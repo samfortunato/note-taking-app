cce0a83a255d61a2781e1fb92c72ae195beee5f8
Samuel Fortunato <samfortunato@gmail.com>
2019-09-27 18:28:47 -0400

    Second iteration

--- index.js
+++ index.js
@@ -6,45 +6,67 @@ class Todo {
   }
 }
 
-class Todos {
+class TodosController {
   static _todos = new Map();
-  static _todosHTMLList = document.querySelector('#todos-list');
 
-  static addTodo(todo) {
+  static getTodo(todoId) {
+    return this._todos.get(todoId);
+  }
+
+  static getAllTodos() {
+    return [...this._todos.values()];
+  }
+
+  static getAllTodoIds() {
+    return [...this._todos.keys()];
+  }
+
+  static postTodo(todo) {
+    const newTodo = new Todo(todo.title, todo.body);
+    this._todos.set(newTodo.id, newTodo);
+  }
+
+  static patchTodo(todo) {
     this._todos.set(todo.id, todo);
   }
 
-  static updateTodo(todo) {
-    this._todos.set(todo.id, todo);
+  static deleteTodo(todoId) {
+    this._todos.delete(todoId);
   }
 
-  static deleteTodo(todo) {
-    this._todos.delete(todo.id);
+  static deleteAllTodos() {    
+    this.getAllTodoIds().forEach(todoId => this._todos.delete(todoId));
   }
 }
 
-class TodosInterface {
-  static _addTodoForm = document.querySelector('#todo-form');
+class TodosView {
+  static todosList = document.querySelector('#todos-list');
+  static todoForm = document.querySelector('#todo-form');
+  static todoFormTodoTitle = document.querySelector('#todo-form #todo-title');
+  static todoFormTodoBody = document.querySelector('#todo-form #todo-body');
 
   static initialize() {
-    this._addTodoForm.onsubmit = this.handleSubmit.bind(this);
+    this.todoForm.addEventListener('submit', this.onPost.bind(this));
   }
-
-  static handleSubmit(evt) {
+  
+  static onPost(evt) {
     evt.preventDefault();
 
+    TodosController.postTodo({
+      title: this.todoFormTodoTitle.value,
+      body: this.todoFormTodoBody.textContent,
+    });
+  }
 
+  static updateTodos() {
+    
   }
 }
 
-class AppEngine {
-  static _initialize() {
-    TodosInterface.initialize();
-  }
+class TodosComponent {
+  static _parts = [TodosController, TodosView];
 
-  static start() {
-    this._initialize();
+  static initialize() {
+    this._parts.forEach(part => { if (part.initialize) part.initialize(); });
   }
 }
-
-AppEngine.start();
