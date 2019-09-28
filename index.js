51808ff93ba034ceb13c5a781a532769c1d60e71
Samuel Fortunato <samfortunato@gmail.com>
2019-09-27 22:48:27 -0400

    Third (working) iteration

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 class Todo {
   constructor(title, body) {
-    this.id = Random.uniqueId();
+    this.id = 1;
     this.title = title;
     this.body = body;
+    this.createdAt = new Date().toLocaleString('en-US', { dateStyle: 'full', timeStyle: 'medium' });
   }
 }
 
-class TodosController {
+class TodosService {
   static _todos = new Map();
 
   static getTodo(todoId) {
     return this._todos.get(todoId);
   }
 
-  static getAllTodos() {
+  static getTodos() {
     return [...this._todos.values()];
   }
 
-  static getAllTodoIds() {
-    return [...this._todos.keys()];
-  }
-
   static postTodo(todo) {
-    const newTodo = new Todo(todo.title, todo.body);
-    this._todos.set(newTodo.id, newTodo);
+    this._todos.set(todo.id, todo);
   }
 
   static patchTodo(todo) {
@@ -33,40 +29,59 @@ class TodosController {
   static deleteTodo(todoId) {
     this._todos.delete(todoId);
   }
-
-  static deleteAllTodos() {    
-    this.getAllTodoIds().forEach(todoId => this._todos.delete(todoId));
-  }
-}
-
-class TodosView {
-  static todosList = document.querySelector('#todos-list');
-  static todoForm = document.querySelector('#todo-form');
-  static todoFormTodoTitle = document.querySelector('#todo-form #todo-title');
-  static todoFormTodoBody = document.querySelector('#todo-form #todo-body');
-
-  static initialize() {
-    this.todoForm.addEventListener('submit', this.onPost.bind(this));
-  }
-  
-  static onPost(evt) {
-    evt.preventDefault();
-
-    TodosController.postTodo({
-      title: this.todoFormTodoTitle.value,
-      body: this.todoFormTodoBody.textContent,
-    });
-  }
-
-  static updateTodos() {
-    
-  }
 }
 
 class TodosComponent {
-  static _parts = [TodosController, TodosView];
+  static _todosList = document.querySelector('#todos-list');
+  static _todoForm = document.querySelector('#todo-form');
+  static _todoFormTitle = document.querySelector('#todo-form-title');
+  static _todoFormBody = document.querySelector('#todo-form-body');
 
   static initialize() {
-    this._parts.forEach(part => { if (part.initialize) part.initialize(); });
+    this._todosList.innerHTML = '';
+    this._todosList.append(...TodosService.getTodos());
+    
+    this._todoForm.onsubmit = this._onSubmit.bind(this);
+  }
+
+  static _onSubmit(evt) {
+    evt.preventDefault();
+
+    const newTodo = new Todo(this._todoFormTitle.value, this._todoFormBody.value);
+    TodosService.postTodo(newTodo);
+
+    this._addNewTodoElementToList(this._createTodoElement(newTodo));
+  }
+
+  static _createTodoElement(todo) {
+    const newTodoElement = document.createElement('li');
+    newTodoElement.className = 'todo'
+    
+    const todoTitleElement = document.createElement('h3');
+    todoTitleElement.textContent = todo.title;
+    const todoCreatedAtElement = document.createElement('h4');
+    todoCreatedAtElement.textContent = todo.createdAt;
+    const todoBodyElement = document.createElement('p');
+    todoBodyElement.textContent = todo.body;
+
+    newTodoElement.append(todoTitleElement, todoCreatedAtElement, todoBodyElement);
+
+    return newTodoElement;
+  }
+
+  static _addNewTodoElementToList(todoEle) {
+    this._todosList.prepend(todoEle);
   }
 }
+
+class AppController {
+  static _components = [
+    TodosComponent,
+  ];
+  
+  static start() {
+    this._components.forEach(component => component.initialize());
+  }
+}
+
+AppController.start();
